2575c167b875e2b8ed319762c1c460949af41641
Mvestrotech <contact@ngarnier.fr>
2021-07-08 11:26:37 +0200

    first commit

---
+++ src/main/java/fr/esgi/marketmanager/MarketManagerApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.marketmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/esgi/marketmanager/MarketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.marketmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
